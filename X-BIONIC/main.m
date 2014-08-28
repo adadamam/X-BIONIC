7162ad65b19c9be67919f006cf1de2a91babb8e6
adadamam <charles@Qiuhaosheng.local>
2014-08-28 15:39:34 +0800

    Initial Commit

---
+++ X-BIONIC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  X-BIONIC
+//
+//  Created by Qiuhaosheng on 14-8-28.
+//  Copyright (c) 2014年 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
